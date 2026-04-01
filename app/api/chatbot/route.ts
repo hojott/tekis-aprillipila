3f9d7766ff262d2d84a413f87456f3144e7f9391
Sakari Marttinen <sakkenino@gmail.com>
2026-04-01 16:13:08 +0300

    Add web_search to api

--- app/api/chatbot/route.ts
+++ app/api/chatbot/route.ts
@@ -18,14 +18,16 @@ export async function POST(req: Request) {
         const stream = new ReadableStream({
             async start(controller) {
                 try {
-                    const completion = await openai.chat.completions.create({
+                    const completion = await openai.responses.create({
                         model,
-                        messages: [{ role: 'user', content: message }],
+                        input: [{ role: 'user', content: message }],
+                        tools: [{type: 'web_search',}],
                         stream: true,
                     });
 
                     for await (const chunk of completion) {
-                        const text = chunk.choices[0]?.delta?.content || '';
+                        console.log(chunk);
+                        const text = chunk.delta || '';
                         controller.enqueue(encoder.encode(text));
                     }
 
